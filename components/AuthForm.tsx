b7598a87c4a85cded0171f88c5db6964ba31e414
Knoka1 <knoka1@hotmail.com>
2024-05-14 23:34:55 -0300

    feature: Adds user actions signUp and signIn structure

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -7,21 +7,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { signUp, signIn } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -36,12 +31,24 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-
-    setIsLoading(false);
+    // try {
+    //   if (type === "sign-up") {
+    //     const newUser = await signUp(data);
+    //     setUser(newUser);
+    //   }
+    //   if (type === "sign-in") {
+    //     const response = await signIn({
+    //       email: data.email,
+    //       password: data.password,
+    //     });
+    //     if (response) router.push("/");
+    //   }
+    // } catch (error) {
+    // } finally {
+    //   setIsLoading(false);
+    // }
   };
   return (
     <section className="auth-form">
@@ -92,9 +99,15 @@ const AuthForm = ({ type }: { type: string }) => {
                   </div>
                   <CustomInput
                     control={form.control}
-                    name="address"
+                    name="address1"
                     label="Address"
                     placeholder="Enter your address"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your city"
                   />
                   <div className="flex gap-4">
                     <CustomInput
--- lib/utils.ts
+++ lib/utils.ts
@@ -202,7 +202,8 @@ export const authFormSchema = (type: string) =>
     //sign up
     firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
     lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
-    address: type === "sign-in" ? z.string().optional() : z.string().min(50),
+    address1: type === "sign-in" ? z.string().optional() : z.string().min(50),
+    city: type === "sign-in" ? z.string().optional() : z.string().min(3),
     state:
       type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
     postalCode:
